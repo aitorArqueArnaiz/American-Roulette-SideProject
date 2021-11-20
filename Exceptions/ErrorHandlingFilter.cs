cc1d8b2f57c1f56fdcef94ccecba3fbdb9269c63
aitoredualdnun <aarque@userzoom.com>
2021-11-20 08:43:49 +0100

    Added basic modules

---
+++ Exceptions/ErrorHandlingFilter.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+public class ErrorHandlingFilter : ExceptionFilterAttribute
+{
+    public override void OnException(ExceptionContext context)
+    {
+        var exception = context.Exception;
+        Serilog.Log.ForContext<ErrorHandlingFilter>().Error($"Error ocurred during execution of action {context.ActionDescriptor.DisplayName}. Error message {exception}");
+
+        context.ExceptionHandled = true; 
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DDDPatterns
 {
@@ -32,6 +26,15 @@ namespace DDDPatterns
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "DDDPatterns", Version = "v1" });
             });
+
+            // Add framework services.
+            services.AddMvc(options =>
+            {
+                options.Filters.Add(new ErrorHandlingFilter());
+            });
+
+            // Add logging service
+            services.AddLogging(configure => configure.AddConsole());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
