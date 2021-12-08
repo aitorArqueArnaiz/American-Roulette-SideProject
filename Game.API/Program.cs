85c7a1a26e672200e81f36c913bb57bb81eea733
aitoredualdnun <aarque@userzoom.com>
2021-12-08 11:15:44 +0100

    Added real connection string for future developement.

--- Game.API/Program.cs
+++ Game.API/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+using System.Text;
 
 namespace Game
 {
@@ -7,6 +10,18 @@ namespace Game
     {
         public static void Main(string[] args)
         {
+            // Create connection string file
+            string path = Directory.GetCurrentDirectory() + "ConnectionString.txt";
+
+            // This text is added only once to the file.
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                string createText = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=Roulette;"
+                                    + "Integrated Security=true;" + Environment.NewLine;
+                File.WriteAllText(path, createText, Encoding.UTF8);
+            }
+
             CreateHostBuilder(args).Build().Run();
         }
 
